567b439212f0a6d42c1bfd8ee19194b0b9368e58
Victor Castro <victor.castro.residente@tre-rn.jus.br>
2024-06-20 18:57:58 -0300

    Funcional

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import json
+import pandas as pd
 
-# Path to the JSON file
-json_file_path = './utils/ufs.json'
+df = pd.read_csv('/opt/trabalhos/etl-mdd/resultado/pbf-2014-2024.csv/part-00000-c0534232-3d57-4dec-bbfe-7deef949e67b-c000.csv')
 
-# Open and read the JSON file directly into a dictionary
-with open(json_file_path, 'r') as file:
-    data_dict = json.load(file)
+# Get the number of rows in the DataFrame, which is equal to the number of lines in the CSV file
+num_lines = df.shape[0]
 
-# Print the dictionary to verify
-print(data_dict)
+# Print the number of lines in the CSV file
+print("Number of lines in the CSV file: ", num_lines)
