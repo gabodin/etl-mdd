d22906386224be0d80fa13e9d83ff2ade1444af2
Victor Castro <vgabriel2211@gmail.com>
2024-06-20 23:35:57 -0300

    Adição de comentários

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/opt/trabalhos/etl-mdd/resultado/pbf-2014-2024.csv/part-00000-c0534232-3d57-4dec-bbfe-7deef949e67b-c000.csv')
-
-# Get the number of rows in the DataFrame, which is equal to the number of lines in the CSV file
-num_lines = df.shape[0]
-
-# Print the number of lines in the CSV file
-print("Number of lines in the CSV file: ", num_lines)
